87aa5050d5f3bf47b201eb8c0554102a583526a3
Marcin Lewandowski <marcin@radiokit.org>
2016-04-03 19:17:51 +0200

    Improved error handling

---
+++ lib/openstax_swift/middleware/temp_url.ex
@@ -0,0 +1,43 @@
+defmodule OpenStax.Swift.Middleware.TempURL do
+  @moduledoc """
+  This module is responsible for generating temporary URLs for objects.
+
+  See http://docs.openstack.org/developer/swift/api/temporary_url_middleware.html
+  """
+
+
+  @doc """
+  Generates temporary public URL for specified object.
+
+  You must set keys as a metadata of container prior to generating such URL.
+  """
+  def generate(backend_id, container, object, key, expires_in) do
+    case OpenStax.Swift.AuthAgent.get_config(backend_id) do
+      nil ->
+        {:error, {:config, :invalid_backend}}
+
+      %{auth_token: auth_token, endpoint_url: endpoint_url} ->
+        case endpoint_url do
+          nil ->
+            {:error, {:auth, :invalid_endpoint}}
+
+          _ ->
+            %URI{path: endpoint_path} = URI.parse(endpoint_url)
+            path = endpoint_path <> "/" <> container <> "/" <> object
+
+            {mega, sec, _} = :os.timestamp
+            unix_timestamp_now = mega * 1000000 + sec
+
+            temp_url_expires = unix_timestamp_now + expires_in
+            temp_url_sig = Base.encode16(:crypto.hmac(:sha, key, "GET\n" <> to_string(temp_url_expires) <> "\n" <> path), case: :lower)
+
+            endpoint_url <> "/" <> container <> "/" <> object <> "?" <>
+              URI.encode_query(%{
+                temp_url_expires: temp_url_expires,
+                temp_url_sig: temp_url_sig
+              })
+
+        end
+    end
+  end
+end
--- lib/openstax_swift/request.ex
+++ lib/openstax_swift/request.ex
@@ -14,35 +14,40 @@ defmodule OpenStax.Swift.Request do
         {:error, {:config, :invalid_backend}}
 
       %{auth_token: auth_token, endpoint_url: endpoint_url} ->
-        case auth_token do
+        case endpoint_url do
           nil ->
-            {:error, {:auth, :unauthorized}}
+            {:error, {:auth, :invalid_endpoint}}
 
-          _ ->
-            headers_full = @request_headers ++ [{"X-Auth-Token", auth_token}]
-            location_full = endpoint_url <> "/" <> String.join(path, "/")
+          case auth_token do
+            nil ->
+              {:error, {:auth, :unauthorized}}
 
-            if headers  != nil, do: headers_full = headers_full ++ headers
-            if metadata != nil, do: headers_full = headers_full ++ metadata # FIXME prefix X-Meta-...
-            if query    != nil, do: location_full = location_full <> "?" <> URI.encode_query(query)
+            _ ->
+              headers_full = @request_headers ++ [{"X-Auth-Token", auth_token}]
+              location_full = endpoint_url <> "/" <> String.join(path, "/")
 
-            case HTTPoison.request(method, location_full, body, headers_full, @request_options) do
-              {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
-                case 401 do
-                  {:error, {:auth, :unauthorized}}
+              if headers  != nil, do: headers_full = headers_full ++ headers
+              if metadata != nil, do: headers_full = headers_full ++ metadata # FIXME prefix X-Meta-...
+              if query    != nil, do: location_full = location_full <> "?" <> URI.encode_query(query)
 
-                case status_code do
-                  expected_status_code ->
-                    {:ok, status_code, body} # TODO parse body
+              case HTTPoison.request(method, location_full, body, headers_full, @request_options) do
+                {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
+                  case 401 do
+                    {:error, {:auth, :unauthorized}}
 
-                  _ ->
-                    {:error, {:httpcode, status_code}}
-                end
+                  case status_code do
+                    expected_status_code ->
+                      {:ok, status_code, body} # TODO parse body
 
-              {:error, reason} ->
-                {:error, {:httperror, reason}}
-            end
-        end
+                    _ ->
+                      {:error, {:httpcode, status_code}}
+                  end
+
+                {:error, reason} ->
+                  {:error, {:httperror, reason}}
+              end
+          end
+      end
     end
   end
 end
