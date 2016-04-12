ab61d2f64fe32e9e92a08ec702c240f833b142d8
Marcin Lewandowski <marcin@radiokit.org>
2016-04-12 13:33:28 +0200

    Allow to use any version of httpoison ~> 1.3

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule OpenStax.Swift.Mixfile do
   defp deps(:test_dev) do
     [
       {:httpoison, "~> 0.8.2"},
-      {:poison},
+      {:poison, "~> 1.3" },
       {:espec, "~> 0.8.17", only: :test}
     ]
   end
